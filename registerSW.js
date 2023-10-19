52c570749d46a180cc36a58ed512ccdcf8b04377
Chirag Rupani <36813124+chiragrupani@users.noreply.github.com>
2023-10-19 22:45:29 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Mysite/sw.js', { scope: '/Mysite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/MySite/sw.js', { scope: '/MySite/' })})}
